543168317795b178dfd06dac021bd69d4313d229
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-10-06 21:10:42 -0400

    switch to node-sass and compile
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- parse-sass.sh
+++ parse-sass.sh
@@ -1,44 +1,44 @@
 #! /bin/bash
 
-if [ ! "$(which sassc 2> /dev/null)" ]; then
-   echo sassc needs to be installed to generate the css.
-   exit 1
+if [ ! "$(which node-sass 2> /dev/null)" ]; then
+    echo node-sass needs to be installed to generate the css.
+    exit 1
 fi
 
-SASSC_OPT="-M -t expanded"
-
 _COLOR_VARIANTS=('' '-light' '-dark')
-if [ ! -z "${COLOR_VARIANTS:-}" ]; then
-  IFS=', ' read -r -a _COLOR_VARIANTS <<< "${COLOR_VARIANTS:-}"
+if [ -n "${COLOR_VARIANTS:-}" ]; then
+    IFS=', ' read -r -a _COLOR_VARIANTS <<< "${COLOR_VARIANTS:-}"
 fi
 
 _ECOLOR_VARIANTS=('' '-dark')
-if [ ! -z "${ECOLOR_VARIANTS:-}" ]; then
-  IFS=', ' read -r -a _ECOLOR_VARIANTS <<< "${ECOLOR_VARIANTS:-}"
+if [ -n "${ECOLOR_VARIANTS:-}" ]; then
+    IFS=', ' read -r -a _ECOLOR_VARIANTS <<< "${ECOLOR_VARIANTS:-}"
 fi
 
 _THEME_VARIANTS=('-sea' '-aliz' '-azul')
-if [ ! -z "${THEME_VARIANTS:-}" ]; then
-  IFS=', ' read -r -a _THEME_VARIANTS <<< "${THEME_VARIANTS:-}"
+if [ -n "${THEME_VARIANTS:-}" ]; then
+    IFS=', ' read -r -a _THEME_VARIANTS <<< "${THEME_VARIANTS:-}"
 fi
 
 for color in "${_COLOR_VARIANTS[@]}"; do
-  for theme in "${_THEME_VARIANTS[@]}"; do
-  sassc $SASSC_OPT src/gtk-3.0/gtk${color}${theme}.{scss,css}
-  echo "==> Generating the gtk${color}${theme}.css..."
-  done
+    for theme in "${_THEME_VARIANTS[@]}"; do
+        node-sass --output-style compressed --sourcemap=none "src/gtk-3.0/gtk${color}${theme}.scss" "src/gtk-3.0/gtk${color}${theme}.css" &
+        echo "==> Generating the gtk${color}${theme}.css..."
+    done
 done
 
 for color in "${_ECOLOR_VARIANTS[@]}"; do
-  for theme in "${_THEME_VARIANTS[@]}"; do
-  sassc $SASSC_OPT src/gnome-shell/gnome-shell${color}${theme}.{scss,css}
-  echo "==> Generating the gnome-shell${color}${theme}.css..."
-  done
+    for theme in "${_THEME_VARIANTS[@]}"; do
+        node-sass --output-style compressed --sourcemap=none "src/gnome-shell/gnome-shell${color}${theme}.scss" "src/gnome-shell/gnome-shell${color}${theme}.css" &
+        echo "==> Generating the gnome-shell${color}${theme}.css..."
+    done
 done
 
 for color in "${_ECOLOR_VARIANTS[@]}"; do
-  for theme in "${_THEME_VARIANTS[@]}"; do
-  sassc $SASSC_OPT src/cinnamon/cinnamon${color}${theme}.{scss,css}
-  echo "==> Generating the cinnamon${color}${theme}.css..."
-  done
+    for theme in "${_THEME_VARIANTS[@]}"; do
+        node-sass --output-style compressed --sourcemap=none "src/cinnamon/cinnamon${color}${theme}.scss" "src/cinnamon/cinnamon${color}${theme}.css" &
+        echo "==> Generating the cinnamon${color}${theme}.css..."
+    done
 done
+
+wait
